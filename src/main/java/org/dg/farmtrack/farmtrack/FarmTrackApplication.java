8c7e3435365c4979128ae13302de4549475af3ae
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-10-01 20:47:41 +0200

    Initial commit

---
+++ src/main/java/org/dg/farmtrack/farmtrack/FarmTrackApplication.java
@@ -0,0 +1,13 @@
+package org.dg.farmtrack.farmtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dg/farmtrack/farmtrack/FarmTrackApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dg.farmtrack.farmtrack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FarmTrackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
